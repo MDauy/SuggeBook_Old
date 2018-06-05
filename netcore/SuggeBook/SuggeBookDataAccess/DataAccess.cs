d3acfa541bbc81c306eebb0339e15c3448d41a42
Marion <marion.daussy@soat.fr>
2018-06-05 14:26:41 +0200

    DataAccess class created

---
+++ netcore/SuggeBook/SuggeBookDataAccess/DataAccess.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuggeBookDataAccess
+{
+	public class DataAccess
+	{
+		private MongoClient _client;
+		private IMongoDatabase _db;
+
+		public DataAccess()
+		{
+			_client = new MongoClient("mongodb://localhost:27017");
+			_db = _client.GetDatabase("SuggeBook");
+			
+		}
+
+	}
+}
