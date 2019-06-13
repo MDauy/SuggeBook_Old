0fe95a152323b7dc1be675db6b976f0145c21b3b
Marion <marion.daussy@gmail.com>
2019-06-13 11:24:15 +0200

    Récupération de l'url redirigée, scrapper à coder

--- SuggeBookScrapper/Program.cs
+++ SuggeBookScrapper/Program.cs
@@ -13,31 +13,48 @@ namespace SuggeBookScrapper
 
         }
 
-        static async void MainAsync (string[] args)
+        static async void MainAsync(string[] args)
         {
             var index = 1;
 
             var baseUrl = "https://www.babelio.com/auteur/xx/";
             var url = $"{baseUrl}{index}";
 
-            using (HttpClientHandler handler = new HttpClientHandler())
+            //var proxy = new WebProxy()
+            //{
+            //    Address = new Uri("http://127.0.0.1:8888")
+            //};
+
+            var handler = new HttpClientHandler();
+
+            using (handler)
             {
-                handler.AllowAutoRedirect = false;
+                handler.AllowAutoRedirect = true;
                 using (HttpClient httpClient = new HttpClient(handler))
-                    using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Head, url))
-                using (var response = await httpClient.SendAsync(request))
-                {
-                    var location = response.Headers.Location;
-                }
+                    while (index < 30)
+                    {
+                        using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url))
+                        using (var response = await httpClient.SendAsync(request))
+                        {
+                            //checker response.RequestMessage
+                            var uri = response.RequestMessage.RequestUri;
+                            if (string.Equals(uri, "https://babelio.com"))
+                            {
+                                index++;
+                                continue;
+                            }
+                            else
+                            {
+                                var content = await httpClient.GetStringAsync(uri);
+                                Console.WriteLine(content);
+                                //Récupérer le nom
+                                //appeler la google api
+                                Console.ReadLine();
+                                index++;
+                            }
+                        }
+                    }
             }
-
-
-                //    var httpClient = new HttpClient();
-                //var response = await httpClient.GetAsync(url);
-                /*var html = await httpClient.GetStringAsync($"{baseUrl}{index}")*/
-                
-            index++;
-
         }
     }
 }
