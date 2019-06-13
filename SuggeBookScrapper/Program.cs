abc1f026087863de5969afecce255df350806517
Marion <marion.daussy@gmail.com>
2019-06-13 14:10:27 +0200

    Récupération du nom ok !

--- SuggeBookScrapper/Program.cs
+++ SuggeBookScrapper/Program.cs
@@ -1,7 +1,11 @@
-﻿using Nito.AsyncEx;
+﻿using HtmlAgilityPack;
+using Nito.AsyncEx;
 using System;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace SuggeBookScrapper
 {
@@ -15,10 +19,10 @@ namespace SuggeBookScrapper
 
         static async void MainAsync(string[] args)
         {
-            var index = 1;
+            var index = 6120;
 
             var baseUrl = "https://www.babelio.com/auteur/xx/";
-            var url = $"{baseUrl}{index}";
+            
 
             //var proxy = new WebProxy()
             //{
@@ -31,29 +35,49 @@ namespace SuggeBookScrapper
             {
                 handler.AllowAutoRedirect = true;
                 using (HttpClient httpClient = new HttpClient(handler))
-                    while (index < 30)
+                    while (index < 3000)
                     {
+                        Thread.Sleep(10000);
+                        var url = $"{baseUrl}{index}";
                         using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url))
                         using (var response = await httpClient.SendAsync(request))
                         {
                             //checker response.RequestMessage
                             var uri = response.RequestMessage.RequestUri;
-                            if (string.Equals(uri, "https://babelio.com"))
+                            if (string.Equals(uri, "https://www.babelio.com/"))
                             {
+                                //Console.WriteLine($"Wrong : index : {index}");
                                 index++;
                                 continue;
                             }
                             else
                             {
-                                var content = await httpClient.GetStringAsync(uri);
-                                Console.WriteLine(content);
-                                //Récupérer le nom
-                                //appeler la google api
-                                Console.ReadLine();
+                                using (var content = await httpClient.GetAsync(uri))
+                                {
+                                    var htmlDocument = new HtmlDocument();
+                                    var pageContent = await content.Content.ReadAsStringAsync();
+                                    if (pageContent != null)
+                                    {
+                                        htmlDocument.LoadHtml(pageContent);
+
+
+                                        var authorNameTags = htmlDocument.DocumentNode.SelectSingleNode("(//div[contains(@class, 'livre_header_con')]//h1//a)[1]");
+                                        if (authorNameTags != null)
+                                        {
+                                            var name = Regex.Replace(authorNameTags.InnerHtml, @"\t|\n|\r", "");
+                                            Console.WriteLine(name);
+                                        }
+                                            //Récupérer le nom
+                                            //appeler la google api                                         
+
+                                    }                                    
+                                }
+                                //Console.WriteLine($"Write but wrong : index : {index} : {uri}");
                                 index++;
                             }
                         }
                     }
+                Console.ReadLine();
             }
         }
     }
