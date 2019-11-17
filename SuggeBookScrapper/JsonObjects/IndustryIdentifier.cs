651456e94c4d7c9b91a8941f6ea98d95c8d59db0
Marion Daussy <marion.daussy@gmail.com>
2019-11-17 11:09:05 +0100

    suppression de la solution SuggeBook.Scrapper pour changement de repository

--- SuggeBookScrapper/JsonObjects/IndustryIdentifier.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SuggeBookScrapper.JsonObjects
-{
-    public class IndustryIdentifier
-    {
-        public string Type { get; set; }
-        public string Identifier { get; set; }
-    }
-}
