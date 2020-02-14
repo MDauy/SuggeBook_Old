6c5c957b77ecc57f36a60bbede5a1d1900845b38
Marion Daussy <marion.daussy@gmail.com>
2020-02-14 18:21:10 +0100

    IMissedSagaRepository (type de retour pas bon, passer à "Task")

--- Suggebook.Back/SuggeBook.Domain/Repositories/IMissedSagaRepository.cs
+++ Suggebook.Back/SuggeBook.Domain/Repositories/IMissedSagaRepository.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SuggeBook.Domain.Model;
 using System.Threading.Tasks;
 
 namespace SuggeBook.Domain.Repositories
 {
     public interface IMissedSagaRepository
     {
-        Task Register (string title, string message);
+        Task<MissedSaga> Register (MissedSaga missedSaga);
     }
 }
