13d24224639999b9f63c3c060a86f1ca96fbfcac
Marion Daussy <marion.daussy@gmail.com>
2023-07-02 23:37:29 +0200

    Réflexions sur les recommandations
    Tentative de création d'un algo de recommandations basé sur Microsoft.ML, mais trop complexe pour les besoins.
    Milanote créé pour Suggebook, avec les différentes idées d'algorithmes de recommandations.
    https://app.milanote.com/1Qg41p1ywbgkd2/recommander

--- README.md
+++ README.md
@@ -14,4 +14,20 @@ Front : React
 Back : 
 - develop : hexa
 
+# Journal
+
+02/07/23
+Tentative de création d'un algo de recommandations basé sur Microsoft.ML, mais trop complexe pour les besoins. 
+Milanote créé pour Suggebook, avec les différentes idées d'algorithmes de recommandations.
+https://app.milanote.com/1Qg41p1ywbgkd2/recommander
+
 le 23/04 : suppression de la couche back
+
+
+# Sources
+(02/07/23)
+Machine learning & algo de recommandations:
+https://mdfarragher.medium.com/build-a-product-recommender-using-c-and-ml-net-machine-learning-ab890b802d25 -> utilise une vieille version de Microsoft.ML
+
+Explication de l'algo field-aware-factorization-machines, pour une recommandation basée sur plusieurs critères
+https://towardsdatascience.com/an-intuitive-explanation-of-field-aware-factorization-machines-a8fee92ce29f
--- Suggebook.Back/SuggeBook.Domain/Model/User.cs
+++ Suggebook.Back/SuggeBook.Domain/Model/User.cs
@@ -4,17 +4,30 @@ using System.Text;
 
 namespace SuggeBook.Domain.Model
 {
-    public class User : Oeuvre
+    public class User : BaseModel
     {
         public User()
         {
             WrongProperties = string.Empty;
         }
-        public string Firstname{ get; set; }
-        public string Lastname { get; set; }
-        public string Email{ get; set; }      
-        public string Username { get; set; }
+
+        public User(string firstName, string lastName, string email, string userName)
+        {
+            this.Firstname = firstName;
+            this.Lastname = lastName;
+            this.Email = email;
+            this.Username = userName;
+        }
+        public string Firstname { get; }
+        public string Lastname { get; }
+        public string Email { get; }
+        public string Username { get; }
         public IList<string> FavoriteCategories { get; set; }
+        public IList<Book> LikedBooks { get; set; }
+        public IList<Show> LikedShows { get; set; }
+        // Users to whom user is subscribed
+        public IList<User> SubscribtionsTo{ get; set; }
+
         public override bool TestCreationValidation()
         {
             if (string.IsNullOrEmpty(Email))
